6f86781fc4c23482bbee1ca5fd8e7a9e0985854d
Kevin Jiang <kevin.jiang@lexisnexis.com>
2013-10-15 10:54:55 +0800

    first commit

---
+++ fileutils.go
@@ -0,0 +1,16 @@
+package fileutils
+
+import (
+	"os"
+)
+
+func FileIsExist(file string) (bool, error) {
+	_, err := os.Stat(file)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
